160cf92f9a37b071a983c794df695d57136ad0ff
noxa <noxa@6621ace7-f22e-0410-b6fb-9d57e39219e7>
2007-02-20 19:09:41 +0000

    Skeletons for new video plugins. Also moved some common stuff (types/etc) to NoxaShared.h in the Shared/Include directory. Other headers will go here, such as the API and types used by the video interface.

--- trunk/Shared/Include/NoxaShared.h
+++ trunk/Shared/Include/NoxaShared.h
@@ -4,12 +4,10 @@
 // Licensed under the LGPL - see License.txt in the project root for details
 // ----------------------------------------------------------------------------
 
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently,
-// but are changed infrequently
+typedef unsigned char byte;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef void* ptr;
 
-#pragma once
-
-#include "Options.h"
-
-#include "NoxaShared.h"
+#define SAFEFREE( x ) { if( x != NULL ) free( x ); x = NULL; }
+#define SAFEDELETE( x ) { if( x != NULL ) delete x; x = NULL; }
