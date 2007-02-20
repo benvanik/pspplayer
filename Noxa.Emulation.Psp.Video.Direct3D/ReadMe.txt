0c5b5d1791f96a64e619327933a95fa08bd6f8ee
Ben Vanik <@benvanik>
2007-02-20 19:09:41 +0000

    Skeletons for new video plugins. Also moved some common stuff (types/etc) to NoxaShared.h in the Shared/Include directory. Other headers will go here, such as the API and types used by the video interface.

---
+++ Noxa.Emulation.Psp.Video.Direct3D/ReadMe.txt
@@ -0,0 +1,5 @@
+========================================================================
+    DYNAMIC LINK LIBRARY : Noxa.Emulation.Psp.Video.Direct3D Project Overview
+========================================================================
+
+A native Direct3D video plugin. This uses the new CPU/Video interface.
---
+++ Noxa.Emulation.Psp.Video.OpenGL/Noxa.Emulation.Psp.Video.OpenGL.h
@@ -0,0 +1,119 @@
+// ----------------------------------------------------------------------------
+// PSP Player Emulation Suite
+// Copyright (C) 2006 Ben Vanik (noxa)
+// Licensed under the LGPL - see License.txt in the project root for details
+// ----------------------------------------------------------------------------
+
+#pragma once
+
+using namespace System;
+using namespace System::Collections::Generic;
+using namespace Noxa::Emulation::Psp;
+
+namespace Noxa {
+	namespace Emulation {
+		namespace Psp {
+			namespace Video {
+
+				public ref class OpenGLVideo : IComponent
+				{
+				public:
+					property ComponentType Type
+					{
+						virtual ComponentType get()
+						{
+							return ComponentType::Video;
+						}
+					}
+
+					property String^ Name
+					{
+						virtual String^ get()
+						{
+							return "OpenGL Video";
+						}
+					}
+
+					property System::Version^ Version
+					{
+						virtual System::Version^ get()
+						{
+							return gcnew System::Version( 1, 0 );
+						}
+					}
+
+					property String^ Author
+					{
+						virtual String^ get()
+						{
+							return "Ben Vanik (ben.vanik@gmail.com)";
+						}
+					}
+
+					property String^ Website
+					{
+						virtual String^ get()
+						{
+							return "http://www.noxa.org";
+						}
+					}
+
+					property String^ RssFeed
+					{
+						virtual String^ get()
+						{
+							return "http://www.noxa.org/rss";
+						}
+					}
+
+					property ComponentBuild Build
+					{
+						virtual ComponentBuild get()
+						{
+#ifdef _DEBUG
+							return ComponentBuild::Debug;
+#else
+							return ComponentBuild::Release;
+#endif
+						}
+					}
+
+					property bool IsTestable
+					{
+						virtual bool get()
+						{
+							return true;
+						}
+					}
+
+					virtual IList<ComponentIssue^>^ Test( ComponentParameters^ parameters );
+
+					virtual String^ ToString() override
+					{
+						return this->Name;
+					}
+
+					property bool IsConfigurable
+					{
+						virtual bool get()
+						{
+							return false;
+						}
+					}
+
+					virtual IComponentConfiguration^ CreateConfiguration( ComponentParameters^ parameters )
+					{
+						return nullptr;
+					}
+
+					virtual IComponentInstance^ CreateInstance( IEmulationInstance^ emulator, ComponentParameters^ parameters )
+					{
+						//return gcnew R4000Cpu( emulator, parameters );
+						return nullptr;
+					}
+				};
+
+			}
+		}
+	}
+}
