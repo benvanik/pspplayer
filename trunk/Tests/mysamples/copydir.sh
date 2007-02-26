fc68cd3142ee704499714314769aeb28ff630756
noxa <noxa@6621ace7-f22e-0410-b6fb-9d57e39219e7>
2007-02-26 20:44:16 +0000

    Tried to build a working copydir.sh (that didn't also copy .svn), but I suck at unix. Instead, I'll just manually copy the data ;)

--- trunk/Tests/mysamples/copydir.sh
+++ trunk/Tests/mysamples/copydir.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
+
 DEV=../stick
 mkdir -p $DEV/$1
-cp -R $2 $DEV/$1/
+echo src: $1/$2/ target: $DEV/$1/$2
+find $2/ -not -regex .*\.svn.* -not -wholename $2/ -print -exec cp '{}' $DEV/$1/$2/ \;
 chmod -R 777 $DEV/$1/$2
+
