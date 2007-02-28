55c9354dde2e0a1878336dbe821b781abc96a306
noxa <noxa@6621ace7-f22e-0410-b6fb-9d57e39219e7>
2007-02-28 18:43:48 +0000

    Small change to test calling convention. Bastards lied. Turns out a0-a3 AND t0-t3 are used, and then args spill on to the stack. Not good. Guess I'll be reworking the BIOS afterall :)
    This was found in the investigation of a BIOS call that was getting bogus parameters. It looks like in the other call cases I've just been luck that what has been on the stack has been valid.

--- trunk/Tests/mysamples/sample1/main.c
+++ trunk/Tests/mysamples/sample1/main.c
@@ -10,10 +10,23 @@
 PSP_MODULE_INFO( "sample1", 0, 1, 1 );
 PSP_MAIN_THREAD_ATTR( THREAD_ATTR_USER | THREAD_ATTR_VFPU );
 
+long long test( int a0, int a1, int a2, int a3, int t0, int t1, int t2, int t3, int sp, int 
+yy )
+{
+	long long r = 0;
+	r = 0xCDCDCDCD; // upper
+	r <<= 32;
+	r |= 0xFEFEFEFE; // lower
+
+	return r;
+}
+
 int main( int argc, char *argv[] )
 {
 	//pspDebugScreenInit();
 	//pspDebugScreenPrintf( "Hello World\n" );
 
-	return 0;
+	long long xx = test( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 );
+
+	return (long)xx;
 }
