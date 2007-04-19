1765f9a002db27b358892e12252dcd0a95cd141e
noxa <noxa@6621ace7-f22e-0410-b6fb-9d57e39219e7>
2007-04-19 19:52:24 +0000

    sceRtc and sceCtrl implemented. sceRtc is not all right, but not much seems to use it so it should be ok. sceCtrl is implemented the best it has been so far, I think.
    With this, sample8 runs.
    PSPTris also runs, but is slower than before. Need to figure out why. The interesting thing, I think, is this:
    FPS: 12.35268
    AttemptedFPS: 46.5624
    It shows that the CPU is pumping out 46fps, but the video driver can only process 12. I'm not sure what would have changed since the NativeHLE, as I don't think I've touched anything relating to that.

--- trunk/Noxa.Emulation.Psp.Bios.ManagedHLE/Modules/sceCtrl.cs
+++ trunk/Noxa.Emulation.Psp.Bios.ManagedHLE/Modules/sceCtrl.cs
@@ -14,10 +14,12 @@ using Noxa.Utilities;
 using Noxa.Emulation.Psp;
 using Noxa.Emulation.Psp.Bios;
 using Noxa.Emulation.Psp.Cpu;
+using Noxa.Emulation.Psp.Input;
+using System.Threading;
 
 namespace Noxa.Emulation.Psp.Bios.ManagedHLE.Modules
 {
-	class sceCtrl : Module
+	unsafe class sceCtrl : Module
 	{
 		#region Properties
 
@@ -31,6 +33,19 @@ namespace Noxa.Emulation.Psp.Bios.ManagedHLE.Modules
 
 		#endregion
 
+		private class Sample
+		{
+			public uint Timestamp;
+			public int Buttons;
+			public int AnalogX;
+			public int AnalogY;
+		}
+
+		private Timer _timer;
+		private FastLinkedList<Sample> _buffer;
+		private AutoResetEvent _bufferEvent;
+		private object _bufferSyncRoot;
+
 		#region State Management
 
 		public sceCtrl( Kernel kernel )
@@ -40,41 +55,110 @@ namespace Noxa.Emulation.Psp.Bios.ManagedHLE.Modules
 
 		public override void Start()
 		{
+			_buffer = new FastLinkedList<Sample>();
+			_bufferEvent = new AutoResetEvent( false );
+			_bufferSyncRoot = new object();
+
+			_device = _kernel.Emulator.Input;
+
+			_timer = _kernel.TimerQueue.CreatePeriodicTimer(
+				new TimerCallback( this.PollCallback ), 16,
+				TimerExecutionContext.WorkerThread, false );
 		}
 
 		public override void Stop()
 		{
+			_kernel.TimerQueue.StopTimer( _timer );
+			_timer = null;
+
+			_buffer.Clear();
+		}
+
+		private void PollCallback( Timer timer )
+		{
+			_device.Poll();
+
+			Sample sample = new Sample();
+			sample.Timestamp = ( uint )Environment.TickCount;
+			sample.Buttons = ( int )_device.Buttons;
+			sample.AnalogX = _device.AnalogX;
+			sample.AnalogY = _device.AnalogY;
+			lock( _bufferSyncRoot )
+			{
+				_buffer.Dequeue();
+				_buffer.Enqueue( sample );
+			}
+			_bufferEvent.Set();
+
+			uint oldPressed = _pressedButtons;
+			_pressedButtons = ( uint )_device.Buttons;
+			uint stillPressed = _pressedButtons & oldPressed;
+			_releasedButtons = ~_pressedButtons;
+
+			_makedButtons = _pressedButtons & ~stillPressed;
+			_breakedButtons = oldPressed & ~stillPressed;
 		}
 
 		#endregion
 
-		[NotImplemented]
+		private enum ControlSamplingMode
+		{
+			DigitalOnly = 0,
+			AnalogAndDigital = 1,
+		}
+
+		private IInputDevice _device;
+		private int _sampleCycle = 0;
+		private ControlSamplingMode _sampleMode = ControlSamplingMode.AnalogAndDigital;
+
+		private uint _makedButtons;
+		private uint _breakedButtons;
+		private uint _pressedButtons;
+		private uint _releasedButtons;
+
 		[Stateless]
 		[BiosFunction( 0x6A2774F3, "sceCtrlSetSamplingCycle" )]
 		// SDK location: /ctrl/pspctrl.h:119
 		// SDK declaration: int sceCtrlSetSamplingCycle(int cycle);
-		public int sceCtrlSetSamplingCycle( int cycle ){ return Module.NotImplementedReturn; }
+		public int sceCtrlSetSamplingCycle( int cycle )
+		{
+			int old = _sampleCycle;
+			_sampleCycle = cycle;
+			return old;
+		}
 
-		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x02BAAD91, "sceCtrlGetSamplingCycle" )]
 		// SDK location: /ctrl/pspctrl.h:128
 		// SDK declaration: int sceCtrlGetSamplingCycle(int *pcycle);
-		public int sceCtrlGetSamplingCycle( int pcycle ){ return Module.NotImplementedReturn; }
+		public int sceCtrlGetSamplingCycle( int pcycle )
+		{
+			if( pcycle != 0 )
+				_memory.WriteWord( pcycle, 4, _sampleCycle );
+			return 0;
+		}
 
-		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x1F4011E6, "sceCtrlSetSamplingMode" )]
 		// SDK location: /ctrl/pspctrl.h:137
 		// SDK declaration: int sceCtrlSetSamplingMode(int mode);
-		public int sceCtrlSetSamplingMode( int mode ){ return Module.NotImplementedReturn; }
+		public int sceCtrlSetSamplingMode( int mode )
+		{
+			int old = ( int )_sampleMode;
+			_sampleMode = ( ControlSamplingMode )mode;
+			return old;
+		}
 
-		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xDA6B76A1, "sceCtrlGetSamplingMode" )]
 		// SDK location: /ctrl/pspctrl.h:146
 		// SDK declaration: int sceCtrlGetSamplingMode(int *pmode);
-		public int sceCtrlGetSamplingMode( int pmode ){ return Module.NotImplementedReturn; }
+		public int sceCtrlGetSamplingMode( int pmode )
+		{
+			if( pmode != 0 )
+				_memory.WriteWord( pmode, 4, ( int )_sampleMode );
+			return 0;
+		}
 
 		[NotImplemented]
 		[Stateless]
@@ -85,53 +169,169 @@ namespace Noxa.Emulation.Psp.Bios.ManagedHLE.Modules
 			return Module.NotImplementedReturn;
 		}
 
-		[NotImplemented]
+		private byte* WritePadData( byte* p, Sample sample, bool positive )
+		{
+			byte analogX;
+			byte analogY;
+			if( _sampleMode == ControlSamplingMode.AnalogAndDigital )
+			{
+				float max = ushort.MaxValue;
+				analogX = ( byte )( ( ( ( float )sample.AnalogX / max ) + 0.5f ) * byte.MaxValue );
+				analogY = ( byte )( ( ( ( float )sample.AnalogY / max ) + 0.5f ) * byte.MaxValue );
+			}
+			else
+			{
+				analogX = 0;
+				analogY = 0;
+			}
+
+			*( ( int* )p ) = ( int )sample.Timestamp;
+			if( positive == true )
+				*( ( int* )( p + 4 ) ) = ( int )sample.Buttons;
+			else
+				*( ( int* )( p + 4 ) ) = ~( int )sample.Buttons;
+			*( p + 8 ) = analogX;
+			*( p + 9 ) = analogY;
+			// 6 bytes of junk
+
+			return p + 16;
+		}
+
 		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0x3A622550, "sceCtrlPeekBufferPositive" )]
 		// SDK location: /ctrl/pspctrl.h:148
 		// SDK declaration: int sceCtrlPeekBufferPositive(SceCtrlData *pad_data, int count);
-		public int sceCtrlPeekBufferPositive( int pad_data, int count ){ return Module.NotImplementedReturn; }
+		public int sceCtrlPeekBufferPositive( int pad_data, int count )
+		{
+			if( pad_data != 0 )
+			{
+				byte* p = _memorySystem.Translate( ( uint )pad_data );
+				for( int n = 0; n < count; n++ )
+				{
+					Sample sample;
+					lock( _bufferSyncRoot )
+						sample = _buffer.Dequeue();
+					if( sample == null )
+						return n;
 
-		[NotImplemented]
+					p = WritePadData( p, sample, true );
+				}
+				return count;
+			}
+			return 0;
+		}
+
 		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0xC152080A, "sceCtrlPeekBufferNegative" )]
 		// SDK location: /ctrl/pspctrl.h:150
 		// SDK declaration: int sceCtrlPeekBufferNegative(SceCtrlData *pad_data, int count);
-		public int sceCtrlPeekBufferNegative( int pad_data, int count ){ return Module.NotImplementedReturn; }
+		public int sceCtrlPeekBufferNegative( int pad_data, int count )
+		{
+			if( pad_data != 0 )
+			{
+				byte* p = _memorySystem.Translate( ( uint )pad_data );
+				for( int n = 0; n < count; n++ )
+				{
+					Sample sample;
+					lock( _bufferSyncRoot )
+						sample = _buffer.Dequeue();
+					if( sample == null )
+						return n;
 
-		[NotImplemented]
+					p = WritePadData( p, sample, false );
+				}
+				return count;
+			}
+			return 0;
+		}
+
 		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0x1F803938, "sceCtrlReadBufferPositive" )]
 		// SDK location: /ctrl/pspctrl.h:168
 		// SDK declaration: int sceCtrlReadBufferPositive(SceCtrlData *pad_data, int count);
-		public int sceCtrlReadBufferPositive( int pad_data, int count ){ return Module.NotImplementedReturn; }
+		public int sceCtrlReadBufferPositive( int pad_data, int count )
+		{
+			if( pad_data != 0 )
+			{
+				byte* p = _memorySystem.Translate( ( uint )pad_data );
+				for( int n = 0; n < count; n++ )
+				{
+					Sample sample;
+					do
+					{
+						_bufferEvent.WaitOne();
+						lock( _bufferSyncRoot )
+							sample = _buffer.Dequeue();
+					} while( sample == null );
 
-		[NotImplemented]
+					p = WritePadData( p, sample, true );
+				}
+				return count;
+			}
+			return 0;
+		}
+
 		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0x60B81F86, "sceCtrlReadBufferNegative" )]
 		// SDK location: /ctrl/pspctrl.h:170
 		// SDK declaration: int sceCtrlReadBufferNegative(SceCtrlData *pad_data, int count);
-		public int sceCtrlReadBufferNegative( int pad_data, int count ){ return Module.NotImplementedReturn; }
+		public int sceCtrlReadBufferNegative( int pad_data, int count )
+		{
+			if( pad_data != 0 )
+			{
+				byte* p = _memorySystem.Translate( ( uint )pad_data );
+				for( int n = 0; n < count; n++ )
+				{
+					Sample sample;
+					do
+					{
+						_bufferEvent.WaitOne();
+						lock( _bufferSyncRoot )
+							sample = _buffer.Dequeue();
+					} while( sample == null );
 
-		[NotImplemented]
+					p = WritePadData( p, sample, false );
+				}
+				return count;
+			}
+			return 0;
+		}
+
 		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0xB1D0E5CD, "sceCtrlPeekLatch" )]
 		// SDK location: /ctrl/pspctrl.h:172
 		// SDK declaration: int sceCtrlPeekLatch(SceCtrlLatch *latch_data);
-		public int sceCtrlPeekLatch( int latch_data ){ return Module.NotImplementedReturn; }
+		public int sceCtrlPeekLatch( int latch_data )
+		{
+			uint* ptr = ( uint* )_memorySystem.Translate( ( uint )latch_data );
+			*ptr = _makedButtons;
+			*( ptr + 1 ) = _breakedButtons;
+			*( ptr + 2 ) = _pressedButtons;
+			*( ptr + 3 ) = _releasedButtons;
+			return 0;
+		}
 
-		[NotImplemented]
 		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0x0B588501, "sceCtrlReadLatch" )]
 		// SDK location: /ctrl/pspctrl.h:174
 		// SDK declaration: int sceCtrlReadLatch(SceCtrlLatch *latch_data);
-		public int sceCtrlReadLatch( int latch_data ){ return Module.NotImplementedReturn; }
+		public int sceCtrlReadLatch( int latch_data )
+		{
+			uint* ptr = ( uint* )_memorySystem.Translate( ( uint )latch_data );
+			*ptr = _makedButtons;
+			*( ptr + 1 ) = _breakedButtons;
+			*( ptr + 2 ) = _pressedButtons;
+			*( ptr + 3 ) = _releasedButtons;
+			_makedButtons = 0;
+			_breakedButtons = 0;
+			return 0;
+		}
 
 	}
 }
--- trunk/Noxa.Emulation.Psp.Bios.ManagedHLE/Modules/sceRtc.cs
+++ trunk/Noxa.Emulation.Psp.Bios.ManagedHLE/Modules/sceRtc.cs
@@ -17,7 +17,7 @@ using Noxa.Emulation.Psp.Cpu;
 
 namespace Noxa.Emulation.Psp.Bios.ManagedHLE.Modules
 {
-	class sceRtc : Module
+	unsafe class sceRtc : Module
 	{
 		#region Properties
 
@@ -48,75 +48,134 @@ namespace Noxa.Emulation.Psp.Bios.ManagedHLE.Modules
 
 		#endregion
 
-		[NotImplemented]
+		public const long TICKSPERUS = 10;
+		public const long TICKSPERSEC = 10000000;
+		public const long TICKSPERMIN = 600000000;
+		public const long TICKSPERHOUR = 36000000000;
+		public const long TICKSPERDAY = 864000000000;
+		public const long TICKSPERWEEK = 6048000000000;
+		public const long TICKSPERMONTH = 24192000000000;		// 4 weeks/month?
+		public const long TICKSPERYEAR = 314496000000000;		// 52 weeks/year?
+
+		[SuggestNative]
 		[Stateless]
 		[BiosFunction( 0xC41C2853, "sceRtcGetTickResolution" )]
 		// SDK location: /rtc/psprtc.h:46
 		// SDK declaration: u32 sceRtcGetTickResolution();
-		public int sceRtcGetTickResolution(){ return Module.NotImplementedReturn; }
+		public int sceRtcGetTickResolution()
+		{
+			return ( int )_kernel.TickFrequency;
+		}
 
-		[NotImplemented]
+		[SuggestNative]
+		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0x3F7AD767, "sceRtcGetCurrentTick" )]
 		// SDK location: /rtc/psprtc.h:54
 		// SDK declaration: int sceRtcGetCurrentTick(u64 *tick);
-		public int sceRtcGetCurrentTick( int tick ){ return Module.NotImplementedReturn; }
+		public int sceRtcGetCurrentTick( int tick )
+		{
+			long currentTick;
+			NativeMethods.QueryPerformanceCounter( out currentTick );
+			long* ptick = ( long* )_memorySystem.Translate( ( uint )tick );
+			*ptick = tick;
+			return 0;
+		}
 
-		[NotImplemented]
+		[SuggestNative]
+		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0x4CFA57B0, "sceRtcGetCurrentClock" )]
 		// SDK location: /rtc/psprtc.h:63
 		// SDK declaration: int sceRtcGetCurrentClock(pspTime *time, int tz);
-		public int sceRtcGetCurrentClock( int time, int tz ){ return Module.NotImplementedReturn; }
+		public int sceRtcGetCurrentClock( int time, int tz )
+		{
+			// tz is minutes from utc to adjust time by
+			if( time != 0 )
+				_kernel.WriteTime( ( uint )time, DateTime.UtcNow.AddMinutes( tz ) );
+			return 0;
+		}
 
-		[NotImplemented]
+		[SuggestNative]
+		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0xE7C27D1B, "sceRtcGetCurrentClockLocalTime" )]
 		// SDK location: /rtc/psprtc.h:71
 		// SDK declaration: int sceRtcGetCurrentClockLocalTime(pspTime *time);
-		public int sceRtcGetCurrentClockLocalTime( int time ){ return Module.NotImplementedReturn; }
+		public int sceRtcGetCurrentClockLocalTime( int time )
+		{
+			if( time != 0 )
+				_kernel.WriteTime( ( uint )time, DateTime.Now );
+			return 0;
+		}
 
-		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x34885E0D, "sceRtcConvertUtcToLocalTime" )]
 		// SDK location: /rtc/psprtc.h:80
 		// SDK declaration: int sceRtcConvertUtcToLocalTime(const u64* tickUTC, u64* tickLocal);
-		public int sceRtcConvertUtcToLocalTime( int tickUTC, int tickLocal ){ return Module.NotImplementedReturn; }
+		public int sceRtcConvertUtcToLocalTime( int tickUTC, int tickLocal )
+		{
+			// THIS IS NOT RIGHT
+			// ticks are interpreted different!
+			DateTime dt = new DateTime( _memory.ReadDoubleWord( tickUTC ) );
+			_memory.WriteDoubleWord( tickLocal, dt.ToLocalTime().Ticks );
+			return 0;
+		}
 
-		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x779242A2, "sceRtcConvertLocalTimeToUTC" )]
 		// SDK location: /rtc/psprtc.h:89
 		// SDK declaration: int sceRtcConvertLocalTimeToUTC(const u64* tickLocal, u64* tickUTC);
-		public int sceRtcConvertLocalTimeToUTC( int tickLocal, int tickUTC ){ return Module.NotImplementedReturn; }
+		public int sceRtcConvertLocalTimeToUTC( int tickLocal, int tickUTC )
+		{
+			// THIS IS NOT RIGHT
+			// ticks are interpreted different!
+			DateTime dt = new DateTime( _memory.ReadDoubleWord( tickLocal ) );
+			_memory.WriteDoubleWord( tickUTC, dt.ToUniversalTime().Ticks );
+			return 0;
+		}
 
-		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x42307A17, "sceRtcIsLeapYear" )]
 		// SDK location: /rtc/psprtc.h:97
 		// SDK declaration: int sceRtcIsLeapYear(int year);
-		public int sceRtcIsLeapYear( int year ){ return Module.NotImplementedReturn; }
+		public int sceRtcIsLeapYear( int year )
+		{
+			return DateTime.IsLeapYear( year ) ? 1 : 0;
+		}
 
-		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x05EF322C, "sceRtcGetDaysInMonth" )]
 		// SDK location: /rtc/psprtc.h:106
 		// SDK declaration: int sceRtcGetDaysInMonth(int year, int month);
-		public int sceRtcGetDaysInMonth( int year, int month ){ return Module.NotImplementedReturn; }
+		public int sceRtcGetDaysInMonth( int year, int month )
+		{
+			return DateTime.DaysInMonth( year, month );
+		}
 
-		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x57726BC1, "sceRtcGetDayOfWeek" )]
 		// SDK location: /rtc/psprtc.h:116
 		// SDK declaration: int sceRtcGetDayOfWeek(int year, int month, int day);
-		public int sceRtcGetDayOfWeek( int year, int month, int day ){ return Module.NotImplementedReturn; }
+		public int sceRtcGetDayOfWeek( int year, int month, int day )
+		{
+			// 0 = monday on psp, but 0 = sunday on windows
+			int dayOfWeek = ( int )new DateTime( year, month, day ).DayOfWeek - 1;
+			if( dayOfWeek < 0 )
+				return 6;
+			else
+				return dayOfWeek;
+		}
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x4B1B5E82, "sceRtcCheckValid" )]
 		// SDK location: /rtc/psprtc.h:124
 		// SDK declaration: int sceRtcCheckValid(const pspTime* date);
-		public int sceRtcCheckValid( int date ){ return Module.NotImplementedReturn; }
+		public int sceRtcCheckValid( int date )
+		{
+			return Module.NotImplementedReturn;
+		}
 
 		[NotImplemented]
 		[Stateless]
@@ -180,75 +239,151 @@ namespace Noxa.Emulation.Psp.Bios.ManagedHLE.Modules
 		// manual add - probably like sceRtcGetTick?
 		public int sceRtcGetAccumulativeTime( int date, int tick ){ return Module.NotImplementedReturn; }
 
-		[NotImplemented]
+		[SuggestNative]
+		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0x9ED0AE87, "sceRtcCompareTick" )]
 		// SDK location: /rtc/psprtc.h:151
 		// SDK declaration: int sceRtcCompareTick(const u64* tick1, const u64* tick2);
-		public int sceRtcCompareTick( int tick1, int tick2 ){ return Module.NotImplementedReturn; }
+		public int sceRtcCompareTick( int tick1, int tick2 )
+		{
+			long t1 = *( long* )_memorySystem.Translate( ( uint )tick1 );
+			long t2 = *( long* )_memorySystem.Translate( ( uint )tick2 );
+			if( t1 == t2 )
+				return 0;
+			else if( t1 < t2 )
+				return -1;
+			else
+				return 1;
+		}
 
-		[NotImplemented]
+		[SuggestNative]
+		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0x44F45E05, "sceRtcTickAddTicks" )]
 		// SDK location: /rtc/psprtc.h:161
 		// SDK declaration: int sceRtcTickAddTicks(u64* destTick, const u64* srcTick, u64 numTicks);
-		public int sceRtcTickAddTicks( int destTick, int srcTick, int numTicks ){ return Module.NotImplementedReturn; }
+		public int sceRtcTickAddTicks( int destTick, int srcTick, long numTicks )
+		{
+			long* pd = ( long* )_memorySystem.Translate( ( uint )destTick );
+			long s = *( long* )_memorySystem.Translate( ( uint )srcTick );
+			*pd = s + numTicks;
+			return 0;
+		}
 
-		[NotImplemented]
+		[SuggestNative]
+		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0x26D25A5D, "sceRtcTickAddMicroseconds" )]
 		// SDK location: /rtc/psprtc.h:171
 		// SDK declaration: int sceRtcTickAddMicroseconds(u64* destTick, const u64* srcTick, u64 numMS);
-		public int sceRtcTickAddMicroseconds( int destTick, int srcTick, int numMS ){ return Module.NotImplementedReturn; }
+		public int sceRtcTickAddMicroseconds( int destTick, int srcTick, long numMS )
+		{
+			long* pd = ( long* )_memorySystem.Translate( ( uint )destTick );
+			long s = *( long* )_memorySystem.Translate( ( uint )srcTick );
+			*pd = s + ( numMS * TICKSPERUS );
+			return 0;
+		}
 
-		[NotImplemented]
+		[SuggestNative]
+		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0xF2A4AFE5, "sceRtcTickAddSeconds" )]
 		// SDK location: /rtc/psprtc.h:181
 		// SDK declaration: int sceRtcTickAddSeconds(u64* destTick, const u64* srcTick, u64 numSecs);
-		public int sceRtcTickAddSeconds( int destTick, int srcTick, int numSecs ){ return Module.NotImplementedReturn; }
+		public int sceRtcTickAddSeconds( int destTick, int srcTick, long numSecs )
+		{
+			long* pd = ( long* )_memorySystem.Translate( ( uint )destTick );
+			long s = *( long* )_memorySystem.Translate( ( uint )srcTick );
+			*pd = s + ( numSecs * TICKSPERSEC );
+			return 0;
+		}
 
-		[NotImplemented]
+		[SuggestNative]
+		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0xE6605BCA, "sceRtcTickAddMinutes" )]
 		// SDK location: /rtc/psprtc.h:191
 		// SDK declaration: int sceRtcTickAddMinutes(u64* destTick, const u64* srcTick, u64 numMins);
-		public int sceRtcTickAddMinutes( int destTick, int srcTick, int numMins ){ return Module.NotImplementedReturn; }
+		public int sceRtcTickAddMinutes( int destTick, int srcTick, long numMins )
+		{
+			long* pd = ( long* )_memorySystem.Translate( ( uint )destTick );
+			long s = *( long* )_memorySystem.Translate( ( uint )srcTick );
+			*pd = s + ( numMins * TICKSPERMIN );
+			return 0;
+		}
 
-		[NotImplemented]
+		[SuggestNative]
+		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0x26D7A24A, "sceRtcTickAddHours" )]
 		// SDK location: /rtc/psprtc.h:201
 		// SDK declaration: int sceRtcTickAddHours(u64* destTick, const u64* srcTick, int numHours);
-		public int sceRtcTickAddHours( int destTick, int srcTick, int numHours ){ return Module.NotImplementedReturn; }
+		public int sceRtcTickAddHours( int destTick, int srcTick, int numHours )
+		{
+			long* pd = ( long* )_memorySystem.Translate( ( uint )destTick );
+			long s = *( long* )_memorySystem.Translate( ( uint )srcTick );
+			*pd = s + ( numHours * TICKSPERHOUR );
+			return 0;
+		}
 
-		[NotImplemented]
+		[SuggestNative]
+		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0xE51B4B7A, "sceRtcTickAddDays" )]
 		// SDK location: /rtc/psprtc.h:211
 		// SDK declaration: int sceRtcTickAddDays(u64* destTick, const u64* srcTick, int numDays);
-		public int sceRtcTickAddDays( int destTick, int srcTick, int numDays ){ return Module.NotImplementedReturn; }
+		public int sceRtcTickAddDays( int destTick, int srcTick, int numDays )
+		{
+			long* pd = ( long* )_memorySystem.Translate( ( uint )destTick );
+			long s = *( long* )_memorySystem.Translate( ( uint )srcTick );
+			*pd = s + ( numDays * TICKSPERDAY );
+			return 0;
+		}
 
-		[NotImplemented]
+		[SuggestNative]
+		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0xCF3A2CA8, "sceRtcTickAddWeeks" )]
 		// SDK location: /rtc/psprtc.h:221
 		// SDK declaration: int sceRtcTickAddWeeks(u64* destTick, const u64* srcTick, int numWeeks);
-		public int sceRtcTickAddWeeks( int destTick, int srcTick, int numWeeks ){ return Module.NotImplementedReturn; }
+		public int sceRtcTickAddWeeks( int destTick, int srcTick, int numWeeks )
+		{
+			long* pd = ( long* )_memorySystem.Translate( ( uint )destTick );
+			long s = *( long* )_memorySystem.Translate( ( uint )srcTick );
+			*pd = s + ( numWeeks * TICKSPERWEEK );
+			return 0;
+		}
 
-		[NotImplemented]
+		[SuggestNative]
 		[Stateless]
 		[BiosFunction( 0xDBF74F1B, "sceRtcTickAddMonths" )]
 		// SDK location: /rtc/psprtc.h:232
 		// SDK declaration: int sceRtcTickAddMonths(u64* destTick, const u64* srcTick, int numMonths);
-		public int sceRtcTickAddMonths( int destTick, int srcTick, int numMonths ){ return Module.NotImplementedReturn; }
+		public int sceRtcTickAddMonths( int destTick, int srcTick, int numMonths )
+		{
+			// THIS IS WRONG
+			// should be converting to datetime and adding like that
+			long* pd = ( long* )_memorySystem.Translate( ( uint )destTick );
+			long s = *( long* )_memorySystem.Translate( ( uint )srcTick );
+			*pd = s + ( numMonths * TICKSPERMONTH );
+			return 0;
+		}
 
-		[NotImplemented]
+		[SuggestNative]
 		[Stateless]
 		[BiosFunction( 0x42842C77, "sceRtcTickAddYears" )]
 		// SDK location: /rtc/psprtc.h:242
 		// SDK declaration: int sceRtcTickAddYears(u64* destTick, const u64* srcTick, int numYears);
-		public int sceRtcTickAddYears( int destTick, int srcTick, int numYears ){ return Module.NotImplementedReturn; }
+		public int sceRtcTickAddYears( int destTick, int srcTick, int numYears )
+		{
+			// THIS IS WRONG
+			// should be converting to datetime and adding like that
+			long* pd = ( long* )_memorySystem.Translate( ( uint )destTick );
+			long s = *( long* )_memorySystem.Translate( ( uint )srcTick );
+			*pd = s + ( numYears * TICKSPERYEAR );
+			return 0;
+		}
 
 		[NotImplemented]
 		[Stateless]
