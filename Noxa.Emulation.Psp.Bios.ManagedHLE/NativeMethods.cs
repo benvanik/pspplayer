9807d9f82c7d84b69a4be30388623c09405af5dc
Ben Vanik <@benvanik>
2007-04-19 19:52:24 +0000

    sceRtc and sceCtrl implemented. sceRtc is not all right, but not much seems to use it so it should be ok. sceCtrl is implemented the best it has been so far, I think.
    With this, sample8 runs.
    PSPTris also runs, but is slower than before. Need to figure out why. The interesting thing, I think, is this:
    FPS: 12.35268
    AttemptedFPS: 46.5624
    It shows that the CPU is pumping out 46fps, but the video driver can only process 12. I'm not sure what would have changed since the NativeHLE, as I don't think I've touched anything relating to that.

--- Noxa.Emulation.Psp.Bios.ManagedHLE/NativeMethods.cs
+++ Noxa.Emulation.Psp.Bios.ManagedHLE/NativeMethods.cs
@@ -16,8 +16,44 @@ namespace Noxa.Emulation.Psp.Bios.ManagedHLE
 	[SuppressUnmanagedCodeSecurity]
 	static class NativeMethods
 	{
+		[StructLayout( LayoutKind.Sequential )]
+		public struct SystemTime
+		{
+			public ushort Year;
+			public ushort Month;
+			public ushort DayOfWeek;
+			public ushort Day;
+			public ushort Hour;
+			public ushort Minute;
+			public ushort Second;
+			public ushort Milliseconds;
+		}
+
+		[StructLayout( LayoutKind.Sequential, CharSet = CharSet.Unicode )]
+		public struct TimeZoneInformation
+		{
+			public int bias;
+			[MarshalAs( UnmanagedType.ByValTStr, SizeConst = 32 )]
+			public string standardName;
+			public SystemTime standardDate;
+			public int standardBias;
+			[MarshalAs( UnmanagedType.ByValTStr, SizeConst = 32 )]
+			public string daylightName;
+			public SystemTime daylightDate;
+			public int daylightBias;
+		}
+
+		[DllImport( "kernel32.dll" )]
+		public static extern uint GetTimeZoneInformation( out TimeZoneInformation lpTimeZoneInformation );
+
 		[DllImport( "kernel32.dll" )]
 		public static extern void GetSystemTimeAsFileTime( out ComTypes.FILETIME lpSystemTimeAsFileTime );
+
+		[DllImport( "kernel32.dll" )]
+		public static extern void GetLocalTime( out SystemTime lpSystemTime );
+
+		[DllImport( "kernel32.dll" )]
+		public static extern void GetSystemTime( out SystemTime lpSystemTime );
 
 		[DllImport( "kernel32.dll" )]
 		public static extern bool QueryPerformanceCounter( out long lpPerformanceCount );
