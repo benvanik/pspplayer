5f13515a8722309074d6addb088d6e2476c87184
ector <ector@6621ace7-f22e-0410-b6fb-9d57e39219e7>
2007-05-16 19:46:31 +0000

    Fleshed out the sceSas stubs a bit, added some commentary

--- Noxa.Emulation.Psp.Bios.ManagedHLE/Modules/sceSas.cs
+++ Noxa.Emulation.Psp.Bios.ManagedHLE/Modules/sceSas.cs
@@ -17,7 +17,13 @@ using Noxa.Emulation.Psp.Cpu;
 
 namespace Noxa.Emulation.Psp.Bios.ManagedHLE.Modules
 {
-	//I have seen this module with several different names :(
+	// sceSas* is actually a static wrapper library that calles the real __sceSas*
+	// functions. __sceSas* have an additional parameter in front of most functions, the sasCore
+	// which is normally hidden from sceSas users
+	// but since __sceSas is what's imported, we have to deal with it
+
+	// There are more layers on top of sceSas but we don't need to care
+	// if we emulate sceSas properly.
 
 	unsafe class sceSas : Module
 	{
@@ -50,40 +56,110 @@ namespace Noxa.Emulation.Psp.Bios.ManagedHLE.Modules
 
 		#endregion
 		
-		[NotImplemented]
+
+		//Don't know the difference between these two :P
 		[Stateless]
 		[BiosFunction(0xa3589d81, "__sceSasCore")]
-		public int __sceSasCore() { return Module.NotImplementedReturn; }
+		public int __sceSasCore() 
+		{
+			return 0;
+		}
 
-		[NotImplemented]
+		[Stateless]
+		[BiosFunction(0x42778a9f, "__sceSasInit")]
+		public int __sceSasInit() 
+		{
+			return 0;
+		}
+
 		[Stateless]
 		[BiosFunction(0x68a46b95, "__sceSasGetEndFlag")]
-		public int __sceSasGetEndFlag() { return Module.NotImplementedReturn; }
+		public int __sceSasGetEndFlag() 
+		{
+			//Quick hack for now, helps puyo puyo in Potemkin
+			return -1; //0xFFFFFFFF, all voices done
+		}
 
-		[NotImplemented]
 		[Stateless]
 		[BiosFunction(0x440ca7d8, "__sceSasSetVolume")]
-		public int __sceSasSetVolume() { return Module.NotImplementedReturn; }
+		public int __sceSasSetVolume(int sasCore, int num, int l, int el, int r, int er) 
+		{
+			return 0;
+		}
 
-		[NotImplemented]
 		[Stateless]
 		[BiosFunction(0xad84d37f, "__sceSasSetPitch")]
-		public int __sceSasSetPitch() { return Module.NotImplementedReturn; }
+		public int __sceSasSetPitch(int sasCore, int num, int pitch)
+		{
+			return 0;
+		}
 
-		[NotImplemented]
 		[Stateless]
 		[BiosFunction(0x99944089, "__sceSasSetVoice")]
-		public int __sceSasSetVoice() { return Module.NotImplementedReturn; }
+		public int __sceSasSetVoice(int sasCore, int num, int vagPtr, int size, int loopmode) 
+		{
+			//Real VAG header is 0x30 bytes behind the vagAddr
+			/*
+			//VAG-Depack, hacked by bITmASTER@bigfoot.com
+			//V0.1
+			double f[5][2] = { { 0.0, 0.0 },
+			{   60.0 / 64.0,  0.0 },
+			{  115.0 / 64.0, -52.0 / 64.0 },
+			{   98.0 / 64.0, -55.0 / 64.0 },
+			{  122.0 / 64.0, -60.0 / 64.0 } };
+
+			double samples[28];
+			int predict_nr, shift_factor, flags;
+			int i;
+			int d, s;
+				
+			double s_1 = 0.0;
+			double s_2 = 0.0;
+
+			while( 1 ) 
+			{
+				predict_nr = fgetc( vag );
+				shift_factor = predict_nr & 0xf;
+				predict_nr >>= 4;
+				flags = fgetc( vag );                           // flags
+				if ( flags == 7 )
+					break;              
+				for ( i = 0; i < 28; i += 2 ) 
+				{
+					d = fgetc( vag );
+					s = ( d & 0xf ) << 12;
+					if ( s & 0x8000 )
+						s |= 0xffff0000;
+					samples[i] = (double) ( s >> shift_factor  );
+					s = ( d & 0xf0 ) << 8;
+					if ( s & 0x8000 )
+						s |= 0xffff0000;
+					samples[i+1] = (double) ( s >> shift_factor  );
+				}
+				for ( i = 0; i < 28; i++ ) {
+					samples[i] = samples[i] + s_1 * f[predict_nr][0] + s_2 * f[predict_nr][1];
+					s_2 = s_1;
+					s_1 = samples[i];
+					d = (int) ( samples[i] + 0.5 );
+					fputc( d & 0xff, pcm );
+					fputc( d >> 8, pcm );
+				}
+			}
+			*/
+			return 0; //success
+		}
+
+		[Stateless]
+		[BiosFunction(0x019b25eb, "__sceSasSetADSR")]
+		public int __sceSasSetADSR(int sasCore, int num, int flag, int a, int d, int s, int r)
+		{
+			return 0;
+		}
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction(0xb7660a23, "__sceSasSetNoise")]
 		public int __sceSasSetNoise() { return Module.NotImplementedReturn; }
-
-		[NotImplemented]
-		[Stateless]
-		[BiosFunction(0x019b25eb, "__sceSasSetADSR")]
-		public int __sceSasSetADSR() { return Module.NotImplementedReturn; }
 
 		[NotImplemented]
 		[Stateless]
@@ -105,10 +181,6 @@ namespace Noxa.Emulation.Psp.Bios.ManagedHLE.Modules
 		[BiosFunction(0xcbcd4f79, "__sceSasSetSimpleADSR")]
 		public int __sceSasSetSimpleADSR() { return Module.NotImplementedReturn; }
 
-		[NotImplemented]
-		[Stateless]
-		[BiosFunction(0x42778a9f, "__sceSasInit")]
-		public int __sceSasInit() { return Module.NotImplementedReturn; }
 
 		[NotImplemented]
 		[Stateless]
@@ -147,13 +219,18 @@ namespace Noxa.Emulation.Psp.Bios.ManagedHLE.Modules
 
 		[NotImplemented]
 		[Stateless]
-		[BiosFunction(0x50a14dfc, "sceSasCore_50a14dfc")]
-		public int sceSasCore_50a14dfc() { return Module.NotImplementedReturn; }
+		[BiosFunction(0x787d04d5, "__sceSasSetPause")]
+		public int __sceSasSetPause() { return Module.NotImplementedReturn; }
+
+
+		//These are probably some of __sceSasExit, __sceSasSetSimpleADSR, 
+		//  __sceSasSetEffect[,Param,Type,Volume]
+		//too lazy to hash and check
 
 		[NotImplemented]
 		[Stateless]
-		[BiosFunction(0x787d04d5, "__sceSasSetPause")]
-		public int __sceSasSetPause() { return Module.NotImplementedReturn; }
+		[BiosFunction(0x50a14dfc, "sceSasCore_50a14dfc")]
+		public int sceSasCore_50a14dfc() { return Module.NotImplementedReturn; }
 
 		[NotImplemented]
 		[Stateless]
