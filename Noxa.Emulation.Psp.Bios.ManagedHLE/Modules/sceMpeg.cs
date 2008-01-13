3909806b4c5e35b91778b8a76ea053011489c105
Ben Vanik <@benvanik>
2008-01-13 20:55:21 +0000

    dummy returns if you uncomment PRETENDVALID

--- Noxa.Emulation.Psp.Bios.ManagedHLE/Modules/sceMpeg.cs
+++ Noxa.Emulation.Psp.Bios.ManagedHLE/Modules/sceMpeg.cs
@@ -4,6 +4,8 @@
 // Licensed under the LGPL - see License.txt in the project root for details
 // ----------------------------------------------------------------------------
 
+//#define PRETENDVALID
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -48,195 +50,201 @@ namespace Noxa.Emulation.Psp.Bios.ManagedHLE.Modules
 
 		#endregion
 
+#if PRETENDVALID
+		public const int DummyReturn = 0;
+#else
+		public const int DummyReturn = Module.NotImplementedReturn;
+#endif
+
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x21FF80E4, "sceMpegQueryStreamOffset" )]
-		public int sceMpegQueryStreamOffset(){ return Module.NotImplementedReturn; }
+		public int sceMpegQueryStreamOffset(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x611E9E11, "sceMpegQueryStreamSize" )]
-		public int sceMpegQueryStreamSize(){ return Module.NotImplementedReturn; }
+		public int sceMpegQueryStreamSize(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x682A619B, "sceMpegInit" )]
-		public int sceMpegInit(){ return Module.NotImplementedReturn; }
+		public int sceMpegInit(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x874624D6, "sceMpegFinish" )]
-		public int sceMpegFinish(){ return Module.NotImplementedReturn; }
+		public int sceMpegFinish(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xC132E22F, "sceMpegQueryMemSize" )]
-		public int sceMpegQueryMemSize(){ return Module.NotImplementedReturn; }
+		public int sceMpegQueryMemSize(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xD8C5F121, "sceMpegCreate" )]
-		public int sceMpegCreate(){ return Module.NotImplementedReturn; }
+		public int sceMpegCreate(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x606A4649, "sceMpegDelete" )]
-		public int sceMpegDelete(){ return Module.NotImplementedReturn; }
+		public int sceMpegDelete(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x42560F23, "sceMpegRegistStream" )]
-		public int sceMpegRegistStream(){ return Module.NotImplementedReturn; }
+		public int sceMpegRegistStream(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x591A4AA2, "sceMpegUnRegistStream" )]
-		public int sceMpegUnRegistStream(){ return Module.NotImplementedReturn; }
+		public int sceMpegUnRegistStream(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xA780CF7E, "sceMpegMallocAvcEsBuf" )]
-		public int sceMpegMallocAvcEsBuf(){ return Module.NotImplementedReturn; }
+		public int sceMpegMallocAvcEsBuf(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xCEB870B1, "sceMpegFreeAvcEsBuf" )]
-		public int sceMpegFreeAvcEsBuf(){ return Module.NotImplementedReturn; }
+		public int sceMpegFreeAvcEsBuf(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xF8DCB679, "sceMpegQueryAtracEsSize" )]
-		public int sceMpegQueryAtracEsSize(){ return Module.NotImplementedReturn; }
+		public int sceMpegQueryAtracEsSize(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xC02CF6B5, "sceMpegQueryPcmEsSize" )]
-		public int sceMpegQueryPcmEsSize(){ return Module.NotImplementedReturn; }
+		public int sceMpegQueryPcmEsSize(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x167AFD9E, "sceMpegInitAu" )]
-		public int sceMpegInitAu(){ return Module.NotImplementedReturn; }
+		public int sceMpegInitAu(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x234586AE, "sceMpegChangeGetAvcAuMode" )]
-		public int sceMpegChangeGetAvcAuMode(){ return Module.NotImplementedReturn; }
+		public int sceMpegChangeGetAvcAuMode(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x9DCFB7EA, "sceMpegChangeGetAuMode" )]
-		public int sceMpegChangeGetAuMode(){ return Module.NotImplementedReturn; }
+		public int sceMpegChangeGetAuMode(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xFE246728, "sceMpegGetAvcAu" )]
-		public int sceMpegGetAvcAu(){ return Module.NotImplementedReturn; }
+		public int sceMpegGetAvcAu(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x8C1E027D, "sceMpegGetPcmAu" )]
-		public int sceMpegGetPcmAu(){ return Module.NotImplementedReturn; }
+		public int sceMpegGetPcmAu(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xE1CE83A7, "sceMpegGetAtracAu" )]
-		public int sceMpegGetAtracAu(){ return Module.NotImplementedReturn; }
+		public int sceMpegGetAtracAu(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x500F0429, "sceMpegFlushStream" )]
-		public int sceMpegFlushStream(){ return Module.NotImplementedReturn; }
+		public int sceMpegFlushStream(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x707B7629, "sceMpegFlushAllStream" )]
-		public int sceMpegFlushAllStream(){ return Module.NotImplementedReturn; }
+		public int sceMpegFlushAllStream(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x0E3C2E9D, "sceMpegAvcDecode" )]
-		public int sceMpegAvcDecode(){ return Module.NotImplementedReturn; }
+		public int sceMpegAvcDecode(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x0F6C18D7, "sceMpegAvcDecodeDetail" )]
-		public int sceMpegAvcDecodeDetail(){ return Module.NotImplementedReturn; }
+		public int sceMpegAvcDecodeDetail(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xA11C7026, "sceMpegAvcDecodeMode" )]
-		public int sceMpegAvcDecodeMode(){ return Module.NotImplementedReturn; }
+		public int sceMpegAvcDecodeMode(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x740FCCD1, "sceMpegAvcDecodeStop" )]
-		public int sceMpegAvcDecodeStop(){ return Module.NotImplementedReturn; }
+		public int sceMpegAvcDecodeStop(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x800C44DF, "sceMpegAtracDecode" )]
-		public int sceMpegAtracDecode(){ return Module.NotImplementedReturn; }
+		public int sceMpegAtracDecode(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xD7A29F46, "sceMpegRingbufferQueryMemSize" )]
-		public int sceMpegRingbufferQueryMemSize(){ return Module.NotImplementedReturn; }
+		public int sceMpegRingbufferQueryMemSize(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x37295ED8, "sceMpegRingbufferConstruct" )]
-		public int sceMpegRingbufferConstruct(){ return Module.NotImplementedReturn; }
+		public int sceMpegRingbufferConstruct(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x13407F13, "sceMpegRingbufferDestruct" )]
-		public int sceMpegRingbufferDestruct(){ return Module.NotImplementedReturn; }
+		public int sceMpegRingbufferDestruct(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xB240A59E, "sceMpegRingbufferPut" )]
-		public int sceMpegRingbufferPut(){ return Module.NotImplementedReturn; }
+		public int sceMpegRingbufferPut(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xB5F6DC87, "sceMpegRingbufferAvailableSize" )]
-		public int sceMpegRingbufferAvailableSize(){ return Module.NotImplementedReturn; }
+		public int sceMpegRingbufferAvailableSize(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x11CAB459, "sceMpeg_11CAB459" )]
-		public int sceMpeg_11CAB459(){ return Module.NotImplementedReturn; }
+		public int sceMpeg_11CAB459(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x3C37A7A6, "sceMpeg_3C37A7A6" )]
-		public int sceMpeg_3C37A7A6(){ return Module.NotImplementedReturn; }
+		public int sceMpeg_3C37A7A6(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xB27711A8, "sceMpeg_B27711A8" )]
-		public int sceMpeg_B27711A8(){ return Module.NotImplementedReturn; }
+		public int sceMpeg_B27711A8(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xD4DD6E75, "sceMpeg_D4DD6E75" )]
-		public int sceMpeg_D4DD6E75(){ return Module.NotImplementedReturn; }
+		public int sceMpeg_D4DD6E75(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xC345DED2, "sceMpeg_C345DED2" )]
-		public int sceMpeg_C345DED2(){ return Module.NotImplementedReturn; }
+		public int sceMpeg_C345DED2(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xCF3547A2, "sceMpegAvcDecodeDetail2" )]
-		public int sceMpegAvcDecodeDetail2(){ return Module.NotImplementedReturn; }
+		public int sceMpegAvcDecodeDetail2(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x988E9E12, "sceMpeg_988E9E12" )]
-		public int sceMpeg_988E9E12(){ return Module.NotImplementedReturn; }
+		public int sceMpeg_988E9E12(){ return DummyReturn; }
 	}
 }
 
