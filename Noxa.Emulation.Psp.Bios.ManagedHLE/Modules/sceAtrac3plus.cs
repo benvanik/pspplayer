9fcac0b5195b4a20732afde6ea7c1c41bc1de283
Ben Vanik <@benvanik>
2008-01-13 08:00:27 +0000

    Hacks
    Finding that in a few games I get some weird semaphore calls (wait, signal, etc) with a semaphore ID of 0, even though I'm positive that the create semaphore call returns a valid UID. Not sure if wait(0) or whatever is a special call, or if there is something bad going on...

--- Noxa.Emulation.Psp.Bios.ManagedHLE/Modules/sceAtrac3plus.cs
+++ Noxa.Emulation.Psp.Bios.ManagedHLE/Modules/sceAtrac3plus.cs
@@ -4,6 +4,8 @@
 // Licensed under the LGPL - see License.txt in the project root for details
 // ----------------------------------------------------------------------------
 
+//#define PRETENDVALID
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -67,149 +69,183 @@ namespace Noxa.Emulation.Psp.Bios.ManagedHLE.Modules
 		// 0xe88f759b » sceAtracGetInternalErrorInfo
 		// Not sure if any of these are actually used
 
+#if PRETENDVALID
+		public const int DummyReturn = 0;
+#else
+		public const int DummyReturn = Module.NotImplementedReturn;
+#endif
+
+#if !PRETENDVALID
 		[NotImplemented]
+#endif
 		[Stateless]
 		[BiosFunction( 0xD1F59FDB, "sceAtracStartEntry" )]
-		public int sceAtracStartEntry(){ return Module.NotImplementedReturn; }
+		public int sceAtracStartEntry(){ return DummyReturn; }
 
+#if !PRETENDVALID
 		[NotImplemented]
+#endif
 		[Stateless]
 		[BiosFunction( 0xD5C28CC0, "sceAtracEndEntry" )]
-		public int sceAtracEndEntry(){ return Module.NotImplementedReturn; }
+		public int sceAtracEndEntry(){ return DummyReturn; }
 
-
-		//allocates an Atrac ID (decoding context?)
+		// Allocates an Atrac ID (decoding context?)
+#if !PRETENDVALID
 		[NotImplemented]
+#endif
 		[Stateless]
 		[BiosFunction( 0x780F88D1, "sceAtracGetAtracID" )]
-		public int sceAtracGetAtracID(int codecType){ return Module.NotImplementedReturn; }
+		public int sceAtracGetAtracID(int codecType){ return DummyReturn; }
 
+#if !PRETENDVALID
 		[NotImplemented]
+#endif
 		[Stateless]
 		[BiosFunction( 0x61EB33F5, "sceAtracReleaseAtracID" )]
 		// SDK location: /atrac3/pspatrac3.h:87
 		// SDK declaration: int sceAtracReleaseAtracID(int atracID);
-		public int sceAtracReleaseAtracID( int atracID ){ return Module.NotImplementedReturn; }
+		public int sceAtracReleaseAtracID( int atracID ){ return DummyReturn; }
 
+#if !PRETENDVALID
 		[NotImplemented]
+#endif
 		[Stateless]
 		[BiosFunction( 0x3F6E26B5, "sceAtracSetHalfwayBuffer" )]
-		public int sceAtracSetHalfwayBuffer(){ return Module.NotImplementedReturn; }
+		public int sceAtracSetHalfwayBuffer(){ return DummyReturn; }
 
+#if !PRETENDVALID
 		[NotImplemented]
-		[BiosFunction( 0x0E2A73AB, "sceAtracSetData" )] [Stateless]
+#endif
+		[BiosFunction( 0x0E2A73AB, "sceAtracSetData" )]
+		[Stateless]
 		// manual add
-		public int sceAtracSetData( int atracID, int buf, int bufsize ){ return Module.NotImplementedReturn; }
+		public int sceAtracSetData( int atracID, int buf, int bufsize ){ return DummyReturn; }
 
+#if !PRETENDVALID
 		[NotImplemented]
+#endif
 		[Stateless]
 		[BiosFunction( 0x7A20E7AF, "sceAtracSetDataAndGetID" )]
 		// SDK location: /atrac3/pspatrac3.h:27
 		// SDK declaration: int sceAtracSetDataAndGetID(void *buf, SceSize bufsize);
-		public int sceAtracSetDataAndGetID( int buf, int bufsize ){ return Module.NotImplementedReturn; }
+		public int sceAtracSetDataAndGetID( int buf, int bufsize ){ return DummyReturn; }
 
+#if !PRETENDVALID
 		[NotImplemented]
+#endif
 		[Stateless]
 		[BiosFunction( 0x0FAE370E, "sceAtracSetHalfwayBufferAndGetID" )]
-		public int sceAtracSetHalfwayBufferAndGetID(){ return Module.NotImplementedReturn; }
+		public int sceAtracSetHalfwayBufferAndGetID(){ return DummyReturn; }
 
-		[DontTrace]
+#if !PRETENDVALID
 		[NotImplemented]
+#endif
+		[DontTrace]
 		[Stateless]
 		[BiosFunction( 0x6A8C3CD5, "sceAtracDecodeData" )]
 		// SDK location: /atrac3/pspatrac3.h:43
 		// SDK declaration: int sceAtracDecodeData(int atracID, u16 *outSamples, int *outN, int *outEnd, int *outRemainFrame);
-		public int sceAtracDecodeData( int atracID, int outSamples, int outN, int outEnd, int outRemainFrame ){ return Module.NotImplementedReturn; }
+		public int sceAtracDecodeData( int atracID, int outSamples, int outN, int outEnd, int outRemainFrame ){ return DummyReturn; }
 
+#if !PRETENDVALID
 		[NotImplemented]
+#endif
 		[Stateless]
 		[BiosFunction( 0x9AE849A7, "sceAtracGetRemainFrame" )]
 		// SDK location: /atrac3/pspatrac3.h:55
 		// SDK declaration: int sceAtracGetRemainFrame(int atracID, int *outRemainFrame);
-		public int sceAtracGetRemainFrame( int atracID, int outRemainFrame ){ return Module.NotImplementedReturn; }
+		public int sceAtracGetRemainFrame( int atracID, int outRemainFrame ){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x5D268707, "sceAtracGetStreamDataInfo" )]
-		public int sceAtracGetStreamDataInfo(int atracID){ return Module.NotImplementedReturn; }
+		public int sceAtracGetStreamDataInfo(int atracID){ return DummyReturn; }
 
+#if !PRETENDVALID
 		[NotImplemented]
+#endif
 		[Stateless]
 		[BiosFunction( 0x7DB31251, "sceAtracAddStreamData" )]
-		public int sceAtracAddStreamData(int atracID, int numBytesAdded){ return Module.NotImplementedReturn; }
+		public int sceAtracAddStreamData(int atracID, int numBytesAdded){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x83E85EA0, "sceAtracGetSecondBufferInfo" )]
-		public int sceAtracGetSecondBufferInfo(){ return Module.NotImplementedReturn; }
+		public int sceAtracGetSecondBufferInfo(){ return DummyReturn; }
 
+#if !PRETENDVALID
 		[NotImplemented]
+#endif
 		[Stateless]
 		[BiosFunction( 0x83BF7AFD, "sceAtracSetSecondBuffer" )]
-		public int sceAtracSetSecondBuffer(){ return Module.NotImplementedReturn; }
+		public int sceAtracSetSecondBuffer(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xE23E3A35, "sceAtracGetNextDecodePosition" )]
-		public int sceAtracGetNextDecodePosition(){ return Module.NotImplementedReturn; }
+		public int sceAtracGetNextDecodePosition(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xA2BBA8BE, "sceAtracGetSoundSample" )]
-		public int sceAtracGetSoundSample(){ return Module.NotImplementedReturn; }
+		public int sceAtracGetSoundSample(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x31668BAA, "sceAtracGetChannel" )]
-		public int sceAtracGetChannel(){ return Module.NotImplementedReturn; }
+		public int sceAtracGetChannel(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xD6A5F2F7, "sceAtracGetMaxSample" )]
 		// SDK location: /atrac3/pspatrac3.h:109
 		// SDK declaration: int sceAtracGetMaxSample(int atracID, int *outMax);
-		public int sceAtracGetMaxSample( int atracID, int outMax ){ return Module.NotImplementedReturn; }
+		public int sceAtracGetMaxSample( int atracID, int outMax ){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0x36FAABFB, "sceAtracGetNextSample" )]
 		// SDK location: /atrac3/pspatrac3.h:98
 		// SDK declaration: int sceAtracGetNextSample(int atracID, int *outN);
-		public int sceAtracGetNextSample( int atracID, int outN ){ return Module.NotImplementedReturn; }
+		public int sceAtracGetNextSample( int atracID, int outN ){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xA554A158, "sceAtracGetBitrate" )]
 		// SDK location: /atrac3/pspatrac3.h:66
 		// SDK declaration: int sceAtracGetBitrate(int atracID, int *outBitrate);
-		public int sceAtracGetBitrate( int atracID, int outBitrate ){ return Module.NotImplementedReturn; }
+		public int sceAtracGetBitrate( int atracID, int outBitrate ){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xFAA4F89B, "sceAtracGetLoopStatus" )]
-		public int sceAtracGetLoopStatus(){ return Module.NotImplementedReturn; }
+		public int sceAtracGetLoopStatus(){ return DummyReturn; }
 
+#if !PRETENDVALID
 		[NotImplemented]
+#endif
 		[Stateless]
 		[BiosFunction( 0x868120B5, "sceAtracSetLoopNum" )]
 		// SDK location: /atrac3/pspatrac3.h:77
 		// SDK declaration: int sceAtracSetLoopNum(int atracID, int nloops);
-		public int sceAtracSetLoopNum( int atracID, int nloops ){ return Module.NotImplementedReturn; }
+		public int sceAtracSetLoopNum( int atracID, int nloops ){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xCA3CA3D2, "sceAtracGetBufferInfoForReseting" )]
-		public int sceAtracGetBufferInfoForReseting(){ return Module.NotImplementedReturn; }
+		public int sceAtracGetBufferInfoForReseting(){ return DummyReturn; }
 
+#if !PRETENDVALID
 		[NotImplemented]
+#endif
 		[Stateless]
 		[BiosFunction( 0x644E5607, "sceAtracResetPlayPosition" )]
-		public int sceAtracResetPlayPosition(){ return Module.NotImplementedReturn; }
+		public int sceAtracResetPlayPosition(){ return DummyReturn; }
 
 		[NotImplemented]
 		[Stateless]
 		[BiosFunction( 0xE88F759B, "sceAtracGetInternalErrorInfo" )]
-		public int sceAtracGetInternalErrorInfo(){ return Module.NotImplementedReturn; }
+		public int sceAtracGetInternalErrorInfo(){ return DummyReturn; }
 	}
 }
 
