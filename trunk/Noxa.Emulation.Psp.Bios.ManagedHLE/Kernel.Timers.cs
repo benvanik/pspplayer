4ad2b2fc01152cb36cee630fd4a729732cb72978
ben.vanik <ben.vanik@6621ace7-f22e-0410-b6fb-9d57e39219e7>
2008-01-20 19:36:56 +0000

    Fixed timer use to queue up timer completion events - this may affect performance a bit, but is a lot more reliable. Cleaned up some timer callback issues that were causing the 'already scheduled' errors.

--- trunk/Noxa.Emulation.Psp.Bios.ManagedHLE/Kernel.Timers.cs
+++ trunk/Noxa.Emulation.Psp.Bios.ManagedHLE/Kernel.Timers.cs
@@ -15,38 +15,86 @@ using Noxa.Emulation.Psp.Cpu;
 
 namespace Noxa.Emulation.Psp.Bios.ManagedHLE
 {
+	public delegate void KernelTimerCallback( Timer timer, object state );
+
 	partial class Kernel
 	{
 		public TimerQueue TimerQueue;
+		private FastLinkedList<TimerCompletionEntry> _timerCompletionQueue;
+		private object _timerSyncRoot;
 
 		private void CreateTimerQueue()
 		{
 			Debug.Assert( TimerQueue == null );
 			TimerQueue = new TimerQueue();
+			_timerCompletionQueue = new FastLinkedList<TimerCompletionEntry>();
+			_timerSyncRoot = new object();
 		}
 
 		private void DestroyTimerQueue()
 		{
+			lock( _timerSyncRoot )
+				_timerCompletionQueue.Clear();
 			Debug.Assert( TimerQueue != null );
 			TimerQueue.Dispose();
 			TimerQueue = null;
 		}
 
-		public Timer AddOneShotTimer( TimerCallback timerCallback, object state, uint dueTime )
+		public Timer AddOneShotTimer( KernelTimerCallback timerCallback, object state, uint dueTime )
 		{
-			Timer timer = TimerQueue.CreateOneShotTimer( timerCallback, dueTime, TimerExecutionContext.TimerThread, false );
+			Timer timer = TimerQueue.CreateOneShotTimer( TimerCallback, dueTime, TimerExecutionContext.TimerThread, false );
 			Debug.Assert( timer != null );
-			timer.State = state;
+			timer.State = new object[] { timerCallback, state };
 			return timer;
 		}
 
-		public Timer AddPeriodicTimer( TimerCallback timerCallback, object state, uint dueTime, uint period )
+		private void TimerCallback( Timer timer )
 		{
-			Timer timer = TimerQueue.CreatePeriodicTimer( timerCallback, dueTime, period, TimerExecutionContext.TimerThread, false );
-			Debug.Assert( timer != null );
-			timer.State = state;
-			return timer;
+			KernelTimerCallback realCallback = ( KernelTimerCallback )( ( object[] )timer.State )[ 0 ];
+			object realState = ( ( object[] )timer.State )[ 1 ];
+			lock( _timerSyncRoot )
+				_timerCompletionQueue.Enqueue( new TimerCompletionEntry( timer, realCallback, realState ) );
+			this.Cpu.BreakExecution();
 		}
+
+		private class TimerCompletionEntry
+		{
+			public readonly Timer Timer;
+			public readonly KernelTimerCallback Callback;
+			public readonly object State;
+			public TimerCompletionEntry( Timer timer, KernelTimerCallback callback, object state )
+			{
+				this.Timer = timer;
+				this.Callback = callback;
+				this.State = state;
+			}
+		}
+
+		private void HandleCompletedTimers()
+		{
+			LinkedListEntry<TimerCompletionEntry> e = null;
+			lock( _timerSyncRoot )
+			{
+				if( _timerCompletionQueue.Count == 0 )
+					return;
+				e = _timerCompletionQueue.HeadEntry;
+				_timerCompletionQueue.Clear();
+			}
+			while( e != null )
+			{
+				e.Value.Callback( e.Value.Timer, e.Value.State );
+				e = e.Next;
+			}
+		}
+
+		// Needs to support the queue like one shot
+		//public Timer AddPeriodicTimer( TimerCallback timerCallback, object state, uint dueTime, uint period )
+		//{
+		//    Timer timer = TimerQueue.CreatePeriodicTimer( timerCallback, dueTime, period, TimerExecutionContext.TimerThread, false );
+		//    Debug.Assert( timer != null );
+		//    timer.State = state;
+		//    return timer;
+		//}
 
 		public void CancelTimer( Timer timer )
 		{
